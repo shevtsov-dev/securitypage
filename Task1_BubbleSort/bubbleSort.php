74500d979086bbf239b19a914018bf4c3f0cf10f
shevtsov-dev <roman.shevtsov@gmail.com>
2024-06-01 20:51:10 +0300

    Уменьшил количество переборов

--- Task1_BubbleSort/bubbleSort.php
+++ Task1_BubbleSort/bubbleSort.php
@@ -3,12 +3,12 @@
 $array = [60, 50, 30, 70, 20, 10];
 print_r($array);
 echo "<br>";
-for ($j = 0; $j < count($array) - 1; $j++) {
-    for ($i = 0; $i < count($array) - 1; $i++) {
-        if ($array[$i] > $array[$i + 1]) {
-            $old = $array[$i];
-            $array[$i] = $array[$i + 1];
-            $array[$i + 1] = $old;
+for ($i = 0; $i < count($array) - 1; $i++) {
+    for ($j = 0; $j < count($array) -$i - 1; $j++) {
+        if ($array[$j] > $array[$j + 1]) {
+            $old = $array[$j];
+            $array[$j] = $array[$j + 1];
+            $array[$j + 1] = $old;
         }
     }
 }
