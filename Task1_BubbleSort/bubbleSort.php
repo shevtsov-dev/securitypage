24e6fbd466781cc90b4d91b6b424b44166ca2932
shevtsov-dev <roman.shevtsov@gmail.com>
2024-06-01 11:37:36 +0300

    Пузырьковая сортировка с заданным массивом чисел

--- Task1_BubbleSort/bubbleSort.php
+++ Task1_BubbleSort/bubbleSort.php
@@ -1,2 +1,16 @@
 <?php
 
+$array = [60, 50, 30, 70, 20, 10];
+print_r($array);
+echo "<br>";
+for ($j = 0; $j < count($array) - 1; $j++) {
+    for ($i = 0; $i < count($array) - 1; $i++) {
+        if ($array[$i] > $array[$i + 1]) {
+            $old = $array[$i];
+            $array[$i] = $array[$i + 1];
+            $array[$i + 1] = $old;
+        }
+    }
+}
+
+print_r($array);
